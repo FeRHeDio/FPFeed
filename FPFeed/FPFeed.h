132e0619588d5db0ac6ccab4970337c65284b15d
Fernando Putallaz <ferhedio@gmail.com>
2020-10-01 15:14:04 -0300

    Initial Commit

---
+++ FPFeed/FPFeed.h
@@ -0,0 +1,19 @@
+//
+//  FPFeed.h
+//  FPFeed
+//
+//  Created by Fernando Putallaz on 01/10/2020.
+//  Copyright © 2020 eFePe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FPFeed.
+FOUNDATION_EXPORT double FPFeedVersionNumber;
+
+//! Project version string for FPFeed.
+FOUNDATION_EXPORT const unsigned char FPFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FPFeed/PublicHeader.h>
+
+
