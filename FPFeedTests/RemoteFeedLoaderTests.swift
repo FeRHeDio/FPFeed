da56f1bd5679318873f4f201fae2fef985dceec6
Fernando Putallaz <ferhedio@gmail.com>
2020-10-01 17:59:09 -0300

    Give a URL instead of hardcoding it in the RemoteFeedLoader

--- FPFeedTests/RemoteFeedLoaderTests.swift
+++ FPFeedTests/RemoteFeedLoaderTests.swift
@@ -10,19 +10,25 @@ import XCTest
 
 class RemoteFeedLoader {
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
         self.client = client
+        self.url = url
     }
     
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
+
+
 
 protocol HTTPClient {
     func get(from url: URL)
 }
+
+
 
 class HTTPClientSpy: HTTPClient {
     func get(from url: URL){
@@ -31,24 +37,29 @@ class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
 }
 
+
+
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_DoNotRequestDataFromURL() {
+        let url = URL(string: "http://a-given-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         //arrange -> GIVEN a client and a sut
+        
+        let url = URL(string: "http://a-given-url.com")
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url!, client: client)
         
         //act -> WHEN we invoke sut.load()
         sut.load()
         
         //assert -> THEN assert that a URL request was initiated in the client
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
