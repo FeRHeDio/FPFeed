47318c380a2198277c1941ccdb18022d8c99a9a6
Fernando Putallaz <ferhedio@gmail.com>
2020-10-01 15:46:20 -0300

    Create FeedItem model

---
+++ FPFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  FPFeed
+//
+//  Created by Fernando Putallaz on 01/10/2020.
+//  Copyright © 2020 eFePe. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem{
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
