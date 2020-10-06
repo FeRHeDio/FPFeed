548a36f5e4a457811cdbfe03cd16d1a964a490ae
Fernando Putallaz <ferhedio@gmail.com>
2020-10-06 08:54:12 -0300

    Repalce optional params with enums with succes and failures cases

--- FPFeed/Feed Api/RemoteFeedLoader.swift
+++ FPFeed/Feed Api/RemoteFeedLoader.swift
@@ -9,8 +9,13 @@
 import Foundation
 
 
+public enum HTTPClientResult {
+    case success(HTTPURLResponse)
+    case failure(Error)
+}
+
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemoteFeedLoader {
@@ -28,10 +33,13 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error, response in
-            if response != nil {
+        client.get(from: url) { clientResult in
+            
+            switch clientResult {
+            case .success:
                 completion(.invalidData)
-            } else {
+                
+            case .failure:
                 completion(.connectivity)
             }
         }
