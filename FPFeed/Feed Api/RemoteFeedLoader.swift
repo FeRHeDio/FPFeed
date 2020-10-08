b54472069ed95ce9bb04879d858c065bcfd288d0
Fernando Putallaz <ferhedio@gmail.com>
2020-10-08 18:48:51 -0300

    move all the mapping logic to the feedItemsMapper

--- FPFeed/Feed Api/RemoteFeedLoader.swift
+++ FPFeed/Feed Api/RemoteFeedLoader.swift
@@ -31,14 +31,12 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if let items = try? FeedItemsMapper.map(data, response) {
-                    completion(.success(items))
-                } else {
-                    completion(.failure(.invalidData))
-                }
+                completion(FeedItemsMapper.map(data, response))
             case .failure:
                 completion(.failure(.connectivity))
             }
         }
     }
+    
+    
 }
