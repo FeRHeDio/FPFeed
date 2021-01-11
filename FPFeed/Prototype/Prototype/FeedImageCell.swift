f49243e366165f08e6d1596461e78a3df6719a6a
Fernando Putallaz <ferhedio@gmail.com>
2021-01-11 19:59:47 -0300

    Add FeedImageCell with storyboards wiring

---
+++ FPFeed/Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Fernando Putallaz on 11/01/2021.
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+    
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+    
+    
+}
