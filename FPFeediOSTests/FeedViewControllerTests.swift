724d7eaac970650907d0f22f5a4d6efa511d2996
Fernando Putallaz <ferhedio@gmail.com>
2021-01-12 11:46:43 -0300

    Loader counts and increases the load of the feed

--- FPFeediOSTests/FeedViewControllerTests.swift
+++ FPFeediOSTests/FeedViewControllerTests.swift
@@ -7,17 +7,25 @@
 //
 
 import XCTest
+import UIKit
 
-final class FeedViewController {
+final class FeedViewController: UIViewController {
+    private var loader: FeedViewControllerTests.LoaderSpy?
     
-    init(loader: FeedViewControllerTests.LoaderSpy) {
-        
+    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loader?.load()
+    }
 }
 
 
-final class FeedViewControllerTests: XCTest {
+final class FeedViewControllerTests: XCTestCase {
     
     func test_init_doesNotLoadFeed() {
         let loader = LoaderSpy()
@@ -26,12 +34,22 @@ final class FeedViewControllerTests: XCTest {
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
     // MARK: - Helpers
     
     class LoaderSpy {
         private(set) var loadCallCount: Int = 0
         
+        func load() {
+            loadCallCount += 1
+        }
     }
-    
-    
 }
