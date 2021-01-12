60093a20c3b09453416d977868bf73d20bd4f7da
Fernando Putallaz <ferhedio@gmail.com>
2021-01-12 09:38:22 -0300

    Add shimmering animation while loading image in the prototype app

--- FPFeed/Prototype/Prototype/FeedViewController.swift
+++ FPFeed/Prototype/Prototype/FeedViewController.swift
@@ -14,11 +14,30 @@ struct FeedImageViewModel {
 }
 
 final class FeedViewController: UITableViewController {
-    private let feed = FeedImageViewModel.prototypeFeed
+    private var feed = [FeedImageViewModel]()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        refresh()
+        tableView.setContentOffset(CGPoint(x: 0, y: -tableView.contentInset.top), animated: false)
+    }
+    
+    @IBAction func refresh() {
+        refreshControl?.beginRefreshing()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            if self.feed.isEmpty {
+                self.feed = FeedImageViewModel.prototypeFeed
+                self.tableView.reloadData()
+            }
+            self.refreshControl?.endRefreshing()
+        }
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return feed.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath) as! FeedImageCell
         let model = feed[indexPath.row]
