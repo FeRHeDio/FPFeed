8472d18a4615de542aba8af479b729c5482b9875
Fernando Putallaz <ferhedio@gmail.com>
2021-02-09 14:46:03 -0300

    Move URLSessionHTTPClient to its own file in production

--- FPFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ FPFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -9,28 +9,6 @@
 import XCTest
 import FPFeed
 
-class URLSessionHTTPClient: HTTPClient {
-  private let session: URLSession
-  
-  init(session: URLSession = .shared) {
-    self.session = session
-  }
-  
-  struct UnexpectedValueRepresentation: Error {}
-  
-  func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-    session.dataTask(with: url) { data, response, error in
-      if let error = error {
-        completion(.failure(error))
-      } else if let data = data, let response = response as? HTTPURLResponse {
-        completion(.success(data, response))
-      } else {
-        completion(.failure(UnexpectedValueRepresentation()))
-      }
-    }.resume()
-  }
-}
-
 
 class URLSessionHTTPClientTests: XCTestCase {
   override func setUp() {
