26def9b0df01626c193c6d50eecc077958678de7
Fernando Putallaz <ferhedio@gmail.com>
2021-05-14 08:41:58 -0300

    Move FeedStore to its own file

---
+++ FPFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,17 @@
+//
+//  FeedStore.swift
+//  FPFeed
+//
+//  Created by Fernando Putallaz on 14/05/2021.
+//  Copyright © 2021 eFePe. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedStore {
+  typealias DeletionCompletion = (Error?) -> Void
+  typealias InsertionCompletion = (Error?) -> Void
+
+  func deleteCachedFeed(completion: @escaping DeletionCompletion)
+  func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
