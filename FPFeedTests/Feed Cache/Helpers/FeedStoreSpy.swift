442c1d06c58f099bf0485fbd2cbd467252d5df31
Fernando Putallaz <ferhedio@gmail.com>
2021-05-17 08:09:44 -0300

    Extract FeedCacheSpy into a shared scope to remove duplication

---
+++ FPFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,48 @@
+//
+//  FeedStoreSpy.swift
+//  FPFeedTests
+//
+//  Created by Fernando Putallaz on 17/05/2021.
+//  Copyright © 2021 eFePe. All rights reserved.
+//
+
+import Foundation
+import FPFeed
+
+class FeedStoreSpy: FeedStore {
+  enum ReceivedMessage: Equatable {
+    case deleteCachedFeed
+    case insert([LocalFeedImage], Date)
+  }
+  
+  private(set) var receivedMessages = [ReceivedMessage]()
+  
+  var deletionCompletions = [DeletionCompletion]()
+  var insertionCompletions = [InsertionCompletion]()
+  
+  func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+    deletionCompletions.append(completion)
+    receivedMessages.append(.deleteCachedFeed )
+  }
+  
+  func completeDeletion(with error: Error, at index: Int = 0) {
+    deletionCompletions[index](error)
+  }
+  
+  func completeDeletionSuccesfuly(at index: Int = 0) {
+    deletionCompletions[index](nil)
+  }
+  
+  func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+    insertionCompletions.append(completion)
+    receivedMessages.append(.insert(feed, timestamp))
+  }
+  
+  func completeInsertion(with error: Error, at index: Int = 0) {
+    insertionCompletions[index](error)
+  }
+  
+  func completeinsertionSuccessfully(at index: Int = 0) {
+    insertionCompletions[index](nil)
+  }
+}
--- FPFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ FPFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -17,7 +17,6 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     XCTAssertEqual(store.receivedMessages, [])
   }
   
-  
   //MARK: - Helpers.
   
   private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
@@ -26,44 +25,6 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     trackForMemoryLeak(store, file: file, line: line)
     trackForMemoryLeak(sut, file: file, line: line)
     return (sut, store)
-  }
-  
-  private class FeedStoreSpy: FeedStore {
-    enum ReceivedMessage: Equatable {
-      case deleteCachedFeed
-      case insert([LocalFeedImage], Date)
-    }
-    
-    private(set) var receivedMessages = [ReceivedMessage]()
-    
-    var deletionCompletions = [DeletionCompletion]()
-    var insertionCompletions = [InsertionCompletion]()
-    
-    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-      deletionCompletions.append(completion)
-      receivedMessages.append(.deleteCachedFeed )
-    }
-    
-    func completeDeletion(with error: Error, at index: Int = 0) {
-      deletionCompletions[index](error)
-    }
-    
-    func completeDeletionSuccesfuly(at index: Int = 0) {
-      deletionCompletions[index](nil)
-    }
-    
-    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-      insertionCompletions.append(completion)
-      receivedMessages.append(.insert(feed, timestamp))
-    }
-    
-    func completeInsertion(with error: Error, at index: Int = 0) {
-      insertionCompletions[index](error)
-    }
-    
-    func completeinsertionSuccessfully(at index: Int = 0) {
-      insertionCompletions[index](nil)
-    }
   }
   
 }
