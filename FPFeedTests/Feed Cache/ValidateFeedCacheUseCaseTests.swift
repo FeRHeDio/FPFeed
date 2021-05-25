0bdb35a4c3ba03876f98c1732b62919b4fe60c79
Fernando Putallaz <ferhedio@gmail.com>
2021-05-25 10:48:12 -0300

    Validate cache command does not delete less than seven days old cache

--- FPFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ FPFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -35,6 +35,18 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
     XCTAssertEqual(store.receivedMessages, [.retrieve])
   }
   
+  func test_validateCache_doesNotDeleteLessThanSevenDaysOldCache() {
+    let feed = uniqueImageFeed()
+    let fixedCurrentDate = Date()
+    let lessThanSevenDayOldTimeStamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+    let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+    
+    sut.validateCache()
+    store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDayOldTimeStamp)
+    
+    XCTAssertEqual(store.receivedMessages, [.retrieve])
+  }
+  
   //MARK: - Helpers.
   
   private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
@@ -45,8 +57,33 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
     return (sut, store)
   }
   
-  
   private func anyNSError() -> NSError {
     return NSError(domain: "any error", code: 0)
   }
+  
+  private func uniqueImage() -> FeedImage {
+    return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+  }
+  
+  private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+    let models = [uniqueImage(), uniqueImage()]
+    let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    return (models, local)
+  }
+  
+  private func anyURL() -> URL {
+    return URL(string: "any-url.com")!
+  }
 }
+
+private extension Date {
+  func adding(days: Int) -> Date {
+    return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+  }
+  
+  func adding(seconds: TimeInterval) -> Date {
+    return self + seconds
+  }
+  
+}
+
