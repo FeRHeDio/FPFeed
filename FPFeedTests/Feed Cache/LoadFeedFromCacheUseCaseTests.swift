f5029f817104f428c0ce1d10ef378d70fdf7ff79
Fernando Putallaz <ferhedio@gmail.com>
2021-05-25 11:40:36 -0300

    Validate cache commando does not delete invalid cache after instance has been deallocated.

--- FPFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ FPFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -166,6 +166,17 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
   }
   
+  func test_validateCachce_doesNotDeleteCacheAfterSUTInstanceHasBeenDeallocated() {
+    let store = FeedStoreSpy()
+    var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+    
+    sut?.validateCache()
+    sut = nil
+    store.completeRetrieval(with: anyNSError())
+    
+    XCTAssertEqual(store.receivedMessages, [.retrieve])
+  }
+  
   //MARK: - Helpers.
   
   private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
