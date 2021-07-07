d3c702edaf574cbf32d60c31a82c78c72be955ff
Fernando Putallaz <ferhedio@gmail.com>
2021-07-07 18:02:13 -0300

    Correct Feed Test to use the adding.seconds instead of adding.days.

--- FPFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ FPFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -28,7 +28,7 @@ extension Date {
     return 7
   }
   
-  func adding(days: Int) -> Date {
+  private func adding(days: Int) -> Date {
     return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
   }
 }
--- FPFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ FPFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -67,7 +67,7 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
   func test_load_deliversNoImagesOnExpiredCache() {
     let feed = uniqueImageFeed()
     let fixedCurrentDate = Date()
-    let expiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(days: -1)
+    let expiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds: -1)
     let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
     
     expect(sut, toCompleteWith: .success([]), when: {
