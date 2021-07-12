46f01bb860e7e522978dd080af705871ef5f43fb
Fernando Putallaz <ferhedio@gmail.com>
2021-07-12 08:40:48 -0300

    Retrieve delivers failure on retrieval error (invalid cached data)

--- FPFeedTests/Feed Cache/CodabelFeedStoreTests.swift
+++ FPFeedTests/Feed Cache/CodabelFeedStoreTests.swift
@@ -48,9 +48,13 @@ class CodableFeedStore {
       return completion(.empty)
     }
     
-    let decoder = JSONDecoder()
-    let cache = try! decoder.decode(Cache.self, from: data)
-    completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+    do {
+      let decoder = JSONDecoder()
+      let cache = try decoder.decode(Cache.self, from: data)
+      completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+    } catch {
+      completion(.failure(error))
+    }
   }
   
   func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
@@ -108,6 +112,13 @@ class CodabelFeedStoreTests: XCTestCase {
     expect(sut, toRetrieve: .found(feed: feed, timestamp: timestamp))
   }
    
+  func test_retrieve_deliversFailureOnRetrievalError() {
+    let sut = makeSUT()
+    
+    try! "invalid data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+    
+    expect(sut, toRetrieve: .failure(anyNSError()))
+  }
   
   // MARK: - Helpers
   
@@ -136,7 +147,8 @@ class CodabelFeedStoreTests: XCTestCase {
     
     sut.retrieve { retrieveResult in
       switch (expectedResult, retrieveResult) {
-      case (.empty, .empty):
+      case (.empty, .empty),
+           (.failure, .failure):
         break
         
       case let (.found(expected), .found(retrieved)):
