ad57af8b206ea64993d4254de7d91cba32968c8f
Fernando Putallaz <ferhedio@gmail.com>
2021-07-12 07:56:39 -0300

    Retrieving from non-empty cache twice delivers same found result (no side-effects)

--- FPFeedTests/Feed Cache/CodabelFeedStoreTests.swift
+++ FPFeedTests/Feed Cache/CodabelFeedStoreTests.swift
@@ -139,6 +139,38 @@ class CodabelFeedStoreTests: XCTestCase {
     }
     wait(for: [exp], timeout: 1.0)
   }
+    
+  func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+    let sut = makeSUT()
+    let feed = uniqueImageFeed().local
+    let timestamp = Date()
+    let exp = expectation(description: "Wait for cache retrieval")
+    
+    sut.insert (feed, timestamp: timestamp){ insertionError in
+      XCTAssertNil(insertionError, "Expected feed to be inserted succesfully")
+      
+      sut.retrieve { firstResult in
+        sut.retrieve { secondResult in
+          switch (firstResult, secondResult) {
+          case let (.found(firstFound), .found(secondFound)):
+            XCTAssertEqual(firstFound.feed, feed)
+            XCTAssertEqual(firstFound.timestamp, timestamp)
+            
+            XCTAssertEqual(secondFound.feed, feed)
+            XCTAssertEqual(secondFound.timestamp, timestamp)
+            
+          default:
+            XCTFail("Expected retrieving twice from non empty cache to deliver same found result with feed \(feed) and timestamp \(timestamp), got \(firstResult) and \(secondResult) instead")
+          }
+          
+          exp.fulfill()
+        }
+      }
+    }
+    wait(for: [exp], timeout: 1.0)
+  }
+  
+    
   
   // MARK: - Helpers
   
